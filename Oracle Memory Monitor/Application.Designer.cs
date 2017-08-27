dd35bd5ad0cf383d5f5985e3e230a7aa388e7a47
Roberto <romogo17@gmail.com>
2017-08-27 17:36:39 -0600

    Funcionalidad al 100% Faltan algunos detalles del commit anterior

--- Oracle Memory Monitor/Application.Designer.cs
+++ Oracle Memory Monitor/Application.Designer.cs
@@ -60,8 +60,6 @@
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
-            this.label12 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
             this.label14 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
@@ -149,18 +147,18 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.label4.Location = new System.Drawing.Point(768, 128);
+            this.label4.Location = new System.Drawing.Point(768, 125);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(189, 16);
+            this.label4.Size = new System.Drawing.Size(167, 32);
             this.label4.TabIndex = 5;
-            this.label4.Text = "SQL mas costoso (por tiempo)";
+            this.label4.Text = "SQL mas Reciente en pico\r\nde memoria";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.label5.Location = new System.Drawing.Point(781, 155);
+            this.label5.Location = new System.Drawing.Point(781, 165);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(55, 16);
             this.label5.TabIndex = 6;
@@ -171,7 +169,7 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.SystemColors.Control;
-            this.label6.Location = new System.Drawing.Point(780, 176);
+            this.label6.Location = new System.Drawing.Point(780, 186);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(18, 20);
             this.label6.TabIndex = 7;
@@ -182,7 +180,7 @@
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.label7.Location = new System.Drawing.Point(781, 206);
+            this.label7.Location = new System.Drawing.Point(781, 216);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(50, 16);
             this.label7.TabIndex = 8;
@@ -193,7 +191,7 @@
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.SystemColors.Control;
-            this.label8.Location = new System.Drawing.Point(780, 227);
+            this.label8.Location = new System.Drawing.Point(780, 237);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(18, 20);
             this.label8.TabIndex = 9;
@@ -204,7 +202,7 @@
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.label9.Location = new System.Drawing.Point(781, 259);
+            this.label9.Location = new System.Drawing.Point(781, 269);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(40, 16);
             this.label9.TabIndex = 10;
@@ -215,41 +213,18 @@
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label10.ForeColor = System.Drawing.SystemColors.Control;
-            this.label10.Location = new System.Drawing.Point(780, 280);
+            this.label10.Location = new System.Drawing.Point(780, 290);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(18, 20);
             this.label10.TabIndex = 11;
             this.label10.Text = "0";
-            // 
-            // label12
-            // 
-            this.label12.AutoSize = true;
-            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label12.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.label12.Location = new System.Drawing.Point(781, 361);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(34, 16);
-            this.label12.TabIndex = 13;
-            this.label12.Text = "SQL";
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.ForeColor = System.Drawing.SystemColors.Control;
-            this.label11.Location = new System.Drawing.Point(781, 382);
-            this.label11.MaximumSize = new System.Drawing.Size(166, 81);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(13, 13);
-            this.label11.TabIndex = 14;
-            this.label11.Text = "0";
             // 
             // label13
             // 
             this.label13.AutoSize = true;
             this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label13.ForeColor = System.Drawing.SystemColors.Control;
-            this.label13.Location = new System.Drawing.Point(780, 331);
+            this.label13.Location = new System.Drawing.Point(780, 341);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(18, 20);
             this.label13.TabIndex = 16;
@@ -260,11 +235,11 @@
             this.label14.AutoSize = true;
             this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label14.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.label14.Location = new System.Drawing.Point(781, 310);
+            this.label14.Location = new System.Drawing.Point(781, 320);
             this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(73, 16);
+            this.label14.Size = new System.Drawing.Size(162, 16);
             this.label14.TabIndex = 15;
-            this.label14.Text = "Tiempo (s)";
+            this.label14.Text = "Memoria Compartida (KB)";
             // 
             // Application
             // 
@@ -274,8 +249,6 @@
             this.ClientSize = new System.Drawing.Size(962, 475);
             this.Controls.Add(this.label13);
             this.Controls.Add(this.label14);
-            this.Controls.Add(this.label11);
-            this.Controls.Add(this.label12);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
@@ -401,19 +374,18 @@
 
             SetAxisLimits(now);
 
-            //  definir el pico de memoria cada tiempo
+            //  definir si hay un pico de memoria
             if(usedMemory > Double.Parse(label2.Text))
             {
-                label2.Text = Decimal.Round((decimal)usedMemory, 4).ToString();
+                label2.Text = Decimal.Round((decimal)usedMemory, 10).ToString();
                 //label3.Text = now.ToString("hh:mm");
                 label3.Text = now.ToShortTimeString();
 
-                DataRow row = GetMostExpensiveSql().Tables[0].Rows[0];
+                DataRow row = GetUsersAtAlert().Tables[0].Rows[0];
                 label6.Text = (string)row["USERNAME"];
                 label8.Text = (string)row["SQL_ID"];
                 label10.Text = Convert.ToString(row["HASH_VALUE"]);
-                label11.Text = (string)row["SQL_FULLTEXT"];
-                label13.Text = Convert.ToString(Convert.ToDouble(row["ELAPSED_TIME"])*0.000001);
+                label13.Text = Convert.ToString(Convert.ToDouble(row["MEMORY"])/1024);
             }
 
             // verificar si hay una nueva alerta
@@ -554,7 +526,7 @@
                 // Create and execute the command
                 OracleCommand objCmd = new OracleCommand();
                 objCmd.Connection = objConn;
-                objCmd.CommandText = "get_most_expensive_sql";
+                objCmd.CommandText = "get_recent_sqls_by_user";
                 objCmd.CommandType = CommandType.StoredProcedure;
 
                 // Set parameters
@@ -625,8 +597,6 @@
         private Label label8;
         private Label label9;
         private Label label10;
-        private Label label12;
-        private Label label11;
         private Label label13;
         private Label label14;
 
